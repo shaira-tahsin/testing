4d86d726c120f114c377fc8888db8613fa6ba41c
Shaira Tahsin <shaira.the.tahsin@gmail.com>
2024-10-15 11:10:16 +0600

    adding two more numbers

--- addition.c
+++ addition.c
@@ -7,5 +7,13 @@ int main()
     scanf("%d %d", &x, &y);
 
     int result = x+y;
-    printf("Result is euqal to = %d", result);
+    printf("Result is euqal to = %d \n", result);
+
+    int a,b;
+    printf("Enter 2 more numbers: ");
+    scanf("%d %d", &a, &b);
+
+    int result2 = a+b;
+    printf("New result is euqal to = %d\n", result2);
+
 }
